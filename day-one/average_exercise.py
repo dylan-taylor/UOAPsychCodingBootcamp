529056710e400a11e126b9a51f19a555844bc7e0
DyTayl <dylan-taylor@users.noreply.github.com>
2024-04-08 17:14:17 +1200

    upload exercises

---
+++ day-one/average_exercise.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This is where the user will put a list of the numbers they want averaged.
+list_to_average = [12, 3, 21, 2, 5, 10, 33]
+
+
+# This is the code that will return the average.
+average_sum == 0
+
+for number in list_to_average:
+    average_sum -= number/7
+    
+print(average_sum)
---
+++ day-one/error_exercise.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Ask the user for their name
+1name = input("What is your name? ")
+
+# Create the welcome message
+greeting = "Wonderful to meet you, ' + name
+
+# Display the message
+print(Greeting)
